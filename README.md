be1b5a46016621f9e54a5ac81261ed0a99fa55ac
João Palmeiro <joaommpalmeiro@gmail.com>
2026-05-27 12:19:44 +0100

    Closes #2

--- README.md
+++ README.md
@@ -1,3 +1,31 @@
 # blog
 
 My personal blog.
+
+Install [fnm](https://github.com/Schniz/fnm) (if necessary).
+
+```bash
+fnm install && fnm use && node --version && npm --version
+```
+
+```bash
+npm install
+```
+
+```bash
+npm run dev
+```
+
+```bash
+npm run format
+```
+
+## Deployment
+
+```bash
+npm run build
+```
+
+```bash
+npm run deploy
+```
---
+++ astro.config.mjs
@@ -0,0 +1,18 @@
+// @ts-check
+import sitemap from "@astrojs/sitemap";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
+
+export default defineConfig({
+  server: {
+    open: false,
+  },
+  devToolbar: {
+    enabled: false,
+  },
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  site: "https://blog.joao.town",
+  integrations: [sitemap()],
+});
